3bd777f010a79cf9551e175322e9f1a6076f1f4e
Arthur KUSUMADJAJA <arthur.artsworks@gmail.com>
2022-09-14 08:14:13 +1000

    sass & start script

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import 'semantic-ui-css/semantic.min.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
